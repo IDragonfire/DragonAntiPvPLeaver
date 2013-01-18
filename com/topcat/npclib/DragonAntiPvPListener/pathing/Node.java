088ea2939e14817336405ab7e16a20bf1ac41eea
IDragonfire <bastian616-git@yahoo.de>
2013-01-18 19:40:11 +0100

    Updated to CB1.4.7-R0.1

--- com/topcat/npclib/DragonAntiPvPListener/pathing/Node.java
+++ com/topcat/npclib/DragonAntiPvPListener/pathing/Node.java
@@ -3,11 +3,11 @@ package com.topcat.npclib.DragonAntiPvPListener.pathing;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.server.v1_4_6.AxisAlignedBB;
+import net.minecraft.server.v1_4_R1.AxisAlignedBB;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_4_6.CraftWorld;
+import org.bukkit.craftbukkit.v1_4_R1.CraftWorld;
 
 public class Node { // Holds data about each block we check
 
@@ -38,7 +38,7 @@ public class Node { // Holds data about each block we check
     public void update() {
         this.notsolid = true;
         if (this.b.getType() != Material.AIR) {
-            AxisAlignedBB box = net.minecraft.server.v1_4_6.Block.byId[this.b
+            AxisAlignedBB box = net.minecraft.server.v1_4_R1.Block.byId[this.b
                     .getTypeId()].e(((CraftWorld) this.b.getWorld())
                     .getHandle(), this.b.getX(), this.b.getY(), this.b.getZ());
             if (box != null) {
