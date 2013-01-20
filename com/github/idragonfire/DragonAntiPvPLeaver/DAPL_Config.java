171858f906b1138fb1b97d6b0a2320ff5ddd0f84
IDragonfire <bastian616-git@yahoo.de>
2013-01-20 16:12:55 +0100

    fix config name spelling

--- com/github/idragonfire/DragonAntiPvPLeaver/DAPL_Config.java
+++ com/github/idragonfire/DragonAntiPvPLeaver/DAPL_Config.java
@@ -47,7 +47,7 @@ public class DAPL_Config extends Config {
     public int npc_spawn_ifhitplayer_time = 20;
 
     public boolean npc_spawn_ifhitmonster_active = true;
-    public int npc_spawn_ifhitplayer_monster = 30;
+    public int npc_spawn_ifhitmonster_monster = 30;
 
     // languages
     public String language_npcSpawned = "NPC spawned";
@@ -61,6 +61,6 @@ public class DAPL_Config extends Config {
     public boolean metrics_factionsUsage = true;
 
     public DAPL_Config(Plugin plugin) {
-        this.setFile(plugin);
+        setFile(plugin);
     }
 }
