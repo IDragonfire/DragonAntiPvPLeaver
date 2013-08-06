e86581b126228af673e12a4545ad2276ed4c26d0
IDragonfire <bastian616-git@yahoo.de>
2013-08-06 09:54:36 +0200

    prevent double injections exception if server reload

--- com/github/idragonfire/DragonAntiPvPLeaver/DAPL_Transformer.java
+++ com/github/idragonfire/DragonAntiPvPLeaver/DAPL_Transformer.java
@@ -1,5 +1,7 @@
 package com.github.idragonfire.DragonAntiPvPLeaver;
 
+import java.lang.reflect.Field;
+
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -113,6 +115,17 @@ public class DAPL_Transformer {
             // TODO: implement dynmaic way
             CtClass cc = cp
                     .get("net.minecraft.server.v1_6_R2.PlayerConnection");
+            // check if class already injectsion, e.g. if server reloaded
+            Class<?> currentClass = Class
+                    .forName("net.minecraft.server.v1_6_R2.PlayerConnection");
+            if (currentClass != null) {
+                for (Field field : currentClass.getDeclaredFields()) {
+                    if (field.getName().contains(FIELD_DELAYED)) {
+                        System.out.println("already injected");
+                        return;
+                    }
+                }
+            }
             transform(cc);
             System.out.println("pushed");
         } catch (Exception e) {
