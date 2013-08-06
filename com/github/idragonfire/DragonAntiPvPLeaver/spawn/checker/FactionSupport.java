fa6b82128210d04ca03436ece1b132ffcc7cbf66
IDragonfire <bastian616-git@yahoo.de>
2013-08-06 08:07:13 +0200

    add Factions 2.0 support

--- com/github/idragonfire/DragonAntiPvPLeaver/spawn/checker/FactionSupport.java
+++ com/github/idragonfire/DragonAntiPvPLeaver/spawn/checker/FactionSupport.java
@@ -8,27 +8,40 @@ import com.github.idragonfire.DragonAntiPvPLeaver.api.DSpawnChecker;
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.entity.BoardColls;
 import com.massivecraft.factions.struct.FFlag;
+import com.massivecraft.mcore.ps.PS;
 
 public class FactionSupport implements DSpawnChecker {
+    protected boolean version_2_x;
     protected boolean oldVersion_1_6;
 
     public FactionSupport() {
         Plugin factions = Bukkit.getPluginManager().getPlugin("Factions");
-        oldVersion_1_6 = factions.getDescription().getVersion().startsWith(
-                "1.6");
+        String version = factions.getDescription().getVersion();
+        version_2_x = version.startsWith("2");
+        oldVersion_1_6 = version.startsWith("1.6");
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public boolean canNpcSpawn(Player player) {
-        // TODO: remove old Factions support
-        if (oldVersion_1_6) {
+        if (version_2_x) {
+            com.massivecraft.factions.entity.Faction faction = BoardColls.get()
+                    .getFactionAt(PS.valueOf(player.getLocation()));
+            if (!faction.getFlag(com.massivecraft.factions.FFlag.PVP)
+                    || faction
+                            .getFlag(com.massivecraft.factions.FFlag.PEACEFUL)) {
+                return false;
+            }
+            // TODO: remove old Factions support
+        } else if (oldVersion_1_6) {
             if (Board.getFactionAt(new FLocation(player.getLocation()))
                     .isSafeZone()) {
                 return false;
             }
         } else {
+            // TODO: remove old Factions support
             Faction playerFaction = Board.getFactionAt(new FLocation(player
                     .getLocation()));
             if (!playerFaction.getFlag(FFlag.PVP)
